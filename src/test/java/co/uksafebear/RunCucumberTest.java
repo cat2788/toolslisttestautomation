cdc0909284880ac0c943649f9bb0985156d9303e
mrcharlieb@gmail.com <student@safebear.co.uk>
2019-07-22 15:41:08 +0100

    updates

--- src/test/java/co/uksafebear/RunCucumberTest.java
+++ src/test/java/co/uksafebear/RunCucumberTest.java
@@ -5,6 +5,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+        plugin = {"json:target/cucumber-report/cucumber.json"},
+        tags = "not @to-do"
+        // glue = "path to Stepdefs is not in the same folder",
+        // features = "path to features is not in the co.uk.safebear folder"
+        )
 public class RunCucumberTest {
 }

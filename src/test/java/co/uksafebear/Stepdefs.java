7a1fc824c0016685b442b264740f3e832bd5c921
mrcharlieb@gmail.com <student@safebear.co.uk>
2019-07-22 11:24:33 +0100

    Initial commit

---
+++ src/test/java/co/uksafebear/Stepdefs.java
@@ -0,0 +1,11 @@
+package co.uksafebear;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+
+}

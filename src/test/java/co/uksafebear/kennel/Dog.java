c2002a7a07358d6464bc5801af4c40eb41181a23
mrcharlieb@gmail.com <student@safebear.co.uk>
2019-07-23 12:24:27 +0100

    declared dog variables

---
+++ src/test/java/co/uksafebear/kennel/Dog.java
@@ -0,0 +1,12 @@
+package co.uksafebear.kennel;
+
+public class Dog {
+
+    private int age;
+    private String breed;
+    private double weight;
+
+    public void setBreed(String newBreed) {
+        breed = newBreed;
+    }
+}

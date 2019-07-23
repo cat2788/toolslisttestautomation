36acde38a94d07b512104563e4d2abd9ae5c6c88
mrcharlieb@gmail.com <student@safebear.co.uk>
2019-07-23 15:00:25 +0100

    create static age command

--- src/test/java/co/uksafebear/kennel/Dog.java
+++ src/test/java/co/uksafebear/kennel/Dog.java
@@ -2,7 +2,7 @@ package co.uksafebear.kennel;
 
 public class Dog {
 
-    private int age;
+    private static int age;
     private String breed;
     private double weight;
 
@@ -10,7 +10,20 @@ public class Dog {
         breed = newBreed;
     }
 
-    public double getWeight(){
-        return weight;
+    public String getBreed(){
+        return breed;
+    }
+
+    public void bark(){
+        System.out.println("bark");
+
+    }
+
+    public static void setAge(int newAge){
+        age = newAge;
+    }
+
+    public static int getAge(){
+        return age;
     }
 }

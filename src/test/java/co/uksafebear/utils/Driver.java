a3d54fbb2df1d1c0be241d5501635ec8171a5539
mrcharlieb@gmail.com <student@safebear.co.uk>
2019-07-24 12:31:09 +0100

    updating driver to run headlessly and with no sleep

--- src/test/java/co/uksafebear/utils/Driver.java
+++ src/test/java/co/uksafebear/utils/Driver.java
@@ -18,6 +18,26 @@ public class Driver {
         ChromeOptions chromeOptions;
 
         switch (BROWSER.toUpperCase()){
+
+            case ("CHROME_HEADLESS"):
+                System.out.println("Executing on CHROME HEADLESS");
+
+                chromeOptions = new ChromeOptions();
+
+                //set chrome to run headlessly
+                chromeOptions.addArguments("headless");
+
+                //make sure the window is large and maximised so nothing dissapears off screen
+                //(even in headless mode!)
+                chromeOptions.addArguments("window-size=1920,1080");
+                chromeOptions.addArguments("start-maximised");
+
+                //set up our chrome driver
+                WebDriverManager.chromedriver().setup();
+
+                //return the chrome driver with the chromeOptions set
+                return new ChromeDriver(chromeOptions);
+
             case "CHROME":
                 //Tell the user which browser we're our running tests on
                 System.out.println("Executing on CHROME");

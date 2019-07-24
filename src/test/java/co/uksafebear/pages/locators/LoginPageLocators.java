8b1c24b76019165a89e3c1acb6f6c71e655f72b6
mrcharlieb@gmail.com <student@safebear.co.uk>
2019-07-24 15:04:35 +0100

    added missing bracket

--- src/test/java/co/uksafebear/pages/locators/LoginPageLocators.java
+++ src/test/java/co/uksafebear/pages/locators/LoginPageLocators.java
@@ -8,7 +8,7 @@ public class LoginPageLocators {
 
     private By usernameLocator = By.id("username");
 
-    private By passwordLocator = By.id("password";
+    private By passwordLocator = By.id("password");
 
     private By loginLocator = By.id("enter");
 

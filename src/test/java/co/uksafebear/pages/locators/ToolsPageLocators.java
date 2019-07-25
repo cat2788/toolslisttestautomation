f3c129a9c6a036217581cfb57fd51135bf7ed633
mrcharlieb@gmail.com <student@safebear.co.uk>
2019-07-25 12:17:24 +0100

    corrected xpath for "login successful" message, and finishing step defs for the @when and @then

--- src/test/java/co/uksafebear/pages/locators/ToolsPageLocators.java
+++ src/test/java/co/uksafebear/pages/locators/ToolsPageLocators.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 @Data
 public class ToolsPageLocators {
 
-    private By successfulLoginMessage = By.xpath(".//div[@class='container']/p/b)");
+    private By successfulLoginMessage = By.xpath(".//div[@class='container']/p/b");
 
     // Locator for my Search field
     private By searchFieldLocator = By.xpath(".//input[@placeholder = 'Type the Name or Use of the tool']");
